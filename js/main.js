ec3da34e36383c0758ea22820475699fa3077303
OzKeeper <52452068+OzKeeper@users.noreply.github.com>
2021-04-09 18:59:13 +0200

    70% adaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
   },
 });
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+// if (document.documentElement.scrollWidth <= 640) {
+//   myChannelSlider.destroy();
+//   recommendedChannelSlider.destroy();
+//   recommendedSlider.destroy();
+// }
